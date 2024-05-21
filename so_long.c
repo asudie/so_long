00790f94fb7cc2e21479fc3adb07f85611810dd4
ahsalam <ahsalam@student.42wolfsburg.de>
2024-05-21 09:01:01 +0200

    some progress with drawing maps

--- so_long.c
+++ so_long.c
@@ -16,6 +16,41 @@ int on_keypress(int keysym, t_data *data)
 	return (0);
 } */
 
+void draw_map(t_data *data)
+{
+	int x = 1920 / data->map->map_length;
+	int y = 1080 / data->map->map_height;
+	int i;
+	int j;
+
+	data->textures[0] = mlx_xpm_file_to_image(data->mlx_ptr, "textures/background.xpm", &x, &y);
+	data->textures[1] = mlx_xpm_file_to_image(data->mlx_ptr, "textures/wall.xpm", &x, &y);
+	data->textures[2] = mlx_xpm_file_to_image(data->mlx_ptr, "textures/player.xpm", &x, &y);
+	data->textures[3] = mlx_xpm_file_to_image(data->mlx_ptr, "textures/collect.xpm", &x, &y);
+	data->textures[4] = mlx_xpm_file_to_image(data->mlx_ptr, "textures/exit.xpm", &x, &y);
+
+	i = 0;
+	while (data->map->map_data[i] != NULL) // Check for the end of the map array
+	{
+		j = 0;
+		while (data->map->map_data[i][j] != '\0' && data->map->map_data[i][j] != '\n') // Check for end of line or end of string
+		{
+			if (data->map->map_data[i][j] == '0')
+				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->textures[0], j * x, i * y);
+			if (data->map->map_data[i][j] == '1')
+				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->textures[1], j * x, i * y);
+			if (data->map->map_data[i][j] == 'P')
+				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->textures[2], j * x, i * y);
+			if (data->map->map_data[i][j] == 'C')
+				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->textures[3], j * x, i * y);
+			if (data->map->map_data[i][j] == 'E')
+				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->textures[4], j * x, i * y);
+			j++;
+		}
+		i++;
+	}
+}
+
 int main(void)
 {
 	t_data *data = malloc(sizeof(t_data));
@@ -32,12 +67,17 @@ int main(void)
 	if (!data->win_ptr)
 		return (free(data->mlx_ptr), 1);
 	void *img;
-	int x = 1920 / data->map->map_length;
+	/*int x = 1920 / data->map->map_length;
 	int y = 1080 / data->map->map_height;
 	// img = mlx_new_image(data->mlx_ptr, 1920, 1080);
 
-	img = mlx_xpm_file_to_image(data->mlx_ptr, "textures/player.xpm", &x, &y);
-	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, img, 50, 50);
+	data->textures[0] = mlx_xpm_file_to_image(data->mlx_ptr, "textures/background.xpm", &x, &y);
+	data->textures[1] = mlx_xpm_file_to_image(data->mlx_ptr, "textures/wall.xpm", &x, &y);
+	data->textures[2] = mlx_xpm_file_to_image(data->mlx_ptr, "textures/player.xpm", &x, &y);
+	data->textures[3] = mlx_xpm_file_to_image(data->mlx_ptr, "textures/collect.xpm", &x, &y);
+	data->textures[4] = mlx_xpm_file_to_image(data->mlx_ptr, "textures/exit.xpm", &x, &y); */
+	draw_map(data);
+	// mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, img, 50, 50);
 
 	// Register key release hook
 	// mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &on_keypress, &data);
--- so_long.h
+++ so_long.h
@@ -1,5 +1,5 @@
 #ifndef SO_LONG_H
-# define SO_LONG_H
+#define SO_LONG_H
 
 // #include "minilibx_opengl_20191021/mlx.h"
 #include "gnl/get_next_line.h"
@@ -13,34 +13,31 @@
 #include <X11/keysym.h>
 #include <X11/Xlib.h>
 
-
-
-
 typedef struct s_game_map
 {
-    int     map_height;
-    int     map_length;
-    int     player_position[2];
-    int     game_score;
-    int     max_score;
-    char    **map_data;
-    int     game_over;
-    int     steps;
-    void    *end_img;
-    void    *wall;
-    void    *exit;
-    void    *player;
-    void    *floor;
-    void    *items;
-}   t_game_map;
+    int map_height;
+    int map_length;
+    int player_position[2];
+    int game_score;
+    int max_score;
+    char **map_data;
+    int game_over;
+    int steps;
+    void *end_img;
+    void *wall;
+    void *exit;
+    void *player;
+    void *floor;
+    void *items;
+} t_game_map;
 
 typedef struct s_data
 {
-	void		*mlx_ptr; // MLX pointer
-	void		*win_ptr; // MLX window pointer
-	void		*textures[5]; // MLX image pointers (on the stack)
-	t_game_map		*map; // Map pointer (contains map details - preferably kept on the stack)
-}	t_data;
+    void *mlx_ptr;     // MLX pointer
+    void *win_ptr;     // MLX window pointer
+    void *textures[5]; // MLX image pointers (on the stack)
+    t_game_map *map;   // Map pointer (contains map details - preferably kept on the stack) 0B, 1W, 2P, 3C, 4E
+} t_data;
 
 int parse_map(char *file, t_game_map *my_map);
 int first_check(char *str);
@@ -50,7 +47,8 @@ int check_str(int fd);
 int check_ber(char *str);
 int check_ber(char *str);
 int nl_strlen(char *s);
- int check_paths(int pos_x, int pos_y, t_game_map *my_map);
+int check_paths(int pos_x, int pos_y, t_game_map *my_map);
 int check_items(t_game_map *map);
+void draw_map(t_data *data);
 
 #endif
